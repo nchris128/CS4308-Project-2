604ddd06040a1f5016145e3ec758a7f03a69d55c
LN <niwanoleiko@gmail.com>
2022-03-20 23:08:11 -0400

    parser skeleton

---
+++ parser.py
@@ -0,0 +1,27 @@
+
+# modified welcome.scl file
+
+from Mod3Scanner import readFilename
+
+nextToken = None
+
+print("Please enter a file name: ")     # enter welcome.scl
+filename = input()
+readFilename(filename)
+
+def compile(self, input):
+    # get keywords from input
+
+def keywords(self):
+    # keyword function
+
+def identifier(self):
+    # get identifier
+
+def operators(self):
+    # get operators
+
+def getnexttoke(self):
+    # getnexttoken
+
+# MAIN GOES HERE
